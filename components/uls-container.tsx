9af3848571503206e23ba6a67f5e657cc426e5d3
Alejo Paulon <alejo.paulon@gmail.com>
2023-08-11 12:30:43 -0300

    uls-container full responsive

--- components/uls-container.tsx
+++ components/uls-container.tsx
@@ -6,8 +6,8 @@ import servers from "@/public/icons/Server.png";
 
 export default function UsersLocationServers() {
   return (
-    <div className="max-h-150 mx-32 my-10 flex flex-row justify-around rounded-xl py-10 align-middle shadow-xl">
-      <div className="flex flex-row items-center">
+    <div className="mx-4 flex flex-row justify-evenly rounded-xl align-middle shadow-xl md:mx-10 md:w-[700px] lg:mx-16 lg:w-[900px] lg:py-10 xl:mx-44 xl:w-[1140px]">
+      <div className="flex flex-col items-center space-y-2 pb-6 align-middle md:flex-row lg:flex-row">
         <Image
           src={users}
           alt="users icon"
@@ -15,7 +15,7 @@ export default function UsersLocationServers() {
           width="50"
           className="max-h-[50px]"
         />
-        <div className="ml-8 flex flex-col align-middle">
+        <div className="flex flex-col text-center align-middle md:ml-8">
           <h1 className="text-lg font-extrabold">90+</h1>
           <p>Users</p>
         </div>
@@ -26,6 +26,7 @@ export default function UsersLocationServers() {
         height="100"
         viewBox="0 0 3 129"
         fill="none"
+        className="hidden lg:inline"
       >
         <path
           d="M1.5 1.76257V127.336"
@@ -34,7 +35,7 @@ export default function UsersLocationServers() {
           stroke-linecap="round"
         />
       </svg>
-      <div className="flex flex-row items-center">
+      <div className="flex flex-col items-center space-y-2 pb-6 pl-2 align-middle md:flex-row lg:flex-row">
         <Image
           src={location}
           alt="location icon"
@@ -42,7 +43,7 @@ export default function UsersLocationServers() {
           width="50"
           className="max-h-[50px]"
         />
-        <div className="ml-8 flex flex-col align-middle">
+        <div className="flex flex-col text-center align-middle md:ml-8">
           <h1 className="text-lg font-extrabold">30+</h1>
           <p>Locations</p>
         </div>
@@ -53,6 +54,7 @@ export default function UsersLocationServers() {
         height="100"
         viewBox="0 0 3 129"
         fill="none"
+        className="hidden lg:inline"
       >
         <path
           d="M1.5 1.76257V127.336"
@@ -61,7 +63,7 @@ export default function UsersLocationServers() {
           stroke-linecap="round"
         />
       </svg>
-      <div className="flex flex-row items-center">
+      <div className="flex flex-col items-center space-y-2 pb-6 align-middle md:flex-row lg:flex-row">
         <Image
           src={servers}
           alt="servers icon"
@@ -69,7 +71,7 @@ export default function UsersLocationServers() {
           width="50"
           className="max-h-[50px] "
         />
-        <div className="ml-8 flex flex-col align-middle">
+        <div className="flex flex-col text-center align-middle md:ml-8">
           <h1 className="text-lg font-extrabold">50+</h1>
           <p>Servers</p>
         </div>
