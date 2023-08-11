db4b72c70a4b93c03649eca17ea3429807b25d52
Alejo Paulon <alejo.paulon@gmail.com>
2023-08-11 11:37:35 -0300

    get-started full responsive

--- components/get-started.tsx
+++ components/get-started.tsx
@@ -5,26 +5,32 @@ import get_started from "../public/firstimg.png";
 
 export default function GetStarted() {
   return (
-    <div className="mx-32 mt-20 flex flex-row justify-between">
-      <div className="flex max-w-xl flex-col items-start">
-        <h1 className="text-5xl/relaxed font-semibold">
+    <div className="mx-20 my-10 flex flex-col items-center lg:flex-row xl:mx-32 xl:mt-20 xl:flex-row xl:justify-between">
+      <div className="flex flex-col items-center space-y-6 text-center lg:items-start lg:space-y-2 lg:text-start xl:max-w-xl xl:flex-col">
+        <h1 className="w-[280px] text-3xl font-semibold md:w-[500px] xl:w-[555px] xl:text-5xl/relaxed">
           Want anything to be easy with{" "}
           <span className="font-bold">Lasles VPN.</span>
         </h1>
-        <p className="mb-12 mt-6 text-base">
-          Provide a network for all your needs with ease and fun using{" "}
-          <span className="font-medium">LaslesVPN</span> discover interesting
-          features from us.
-        </p>
-        <Button variant="filled_red" size="lg">
-          Get Started
-        </Button>
+        <div className="md:flex md:flex-row md:space-x-6 lg:flex-col lg:space-x-0 lg:space-y-4">
+          <p className="w-[280px] text-base md:w-[400px] md:text-left xl:mb-12 xl:mt-2 xl:w-[555px] xl:leading-loose">
+            Provide a network for all your needs with ease and fun using{" "}
+            <span className="font-medium">LaslesVPN</span> discover interesting
+            features from us.
+          </p>
+          <Button
+            variant="filled_red"
+            size="lg"
+            className="my-4 w-[250px] md:my-2 xl:my-0"
+          >
+            Get Started
+          </Button>
+        </div>
       </div>
-      <div>
+      <div className="my-10">
         <Image
           src={get_started}
           alt="Image about getting started in LaslesVPN"
-          className="max-h-[350px] "
+          className="min-w-[280px] xl:max-h-[382px]"
         />
       </div>
     </div>
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,23 +1,32 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import logo from "@/public/logo.png";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function Nav() {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
   return (
-    <div className="mx-32 my-10 flex flex-row justify-between">
-      <div className="flex flex-row items-center">
-        <Image src={logo} alt="Logo from LaslesVPN" className="mr-2" />
-        <h1 className="text-center text-xl font-bold">LaslesVPN</h1>
+    <div className="flex xl:mx-32 xl:my-10 xl:flex-row xl:justify-between">
+      <div className="flex xl:flex-row xl:items-center">
+        <Image src={logo} alt="Logo from LaslesVPN" className="xl:mr-2" />
+        <h1 className="text-center font-bold xl:text-xl">LaslesVPN</h1>
       </div>
-      <div className="mt-4 flex flex-row space-x-12">
+      <div className="flex xl:mt-4 xl:flex-row xl:space-x-12">
         <a href="/">About</a>
         <a href="/">Features</a>
         <a href="/">Pricing</a>
         <a href="/">Testimonials</a>
         <a href="/">Help</a>
       </div>
-      <div className="mt-2 flex flex-row space-x-2">
+      <div className="flex xl:mt-2 xl:flex-row xl:space-x-2">
         <Button variant="ghost">Sign In</Button>
         <Button variant="outline_red">Sign Up</Button>
       </div>
