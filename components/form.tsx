b0763cba35165fbc1238881294d377f79441e31d
Alejo Paulon <alejo.paulon@gmail.com>
2023-08-23 18:45:26 -0300

    used react-hook-form and yup, have to style new errors

--- components/form.tsx
+++ components/form.tsx
@@ -1,18 +1,34 @@
 "use client";
 
 import React from "react";
-import { useState } from "react";
 import { Button } from "./ui/button";
 import { BASE_URL } from "@/app/page";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    email: yup
+      .string()
+      .email("Es necesario poner un mail válido")
+      .min(5, "Mínimo 5 caracteres")
+      .required(),
+    name: yup.string().min(5, "Mínimo 5 caracteres").required(),
+    lastName: yup.string().min(5, "Mínimo 5 caracteres").required(),
+  })
+  .required();
+type FormData = yup.InferType<typeof schema>;
 
 export default function Form() {
-  const [data, setData] = useState({
-    email: "",
-    name: "",
-    lastName: "",
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
   });
-
-  const handleFetch = () => {
+  const onSubmit = (data: FormData) => {
     fetch(`${BASE_URL}/api/send-email`, {
       method: "POST",
       body: JSON.stringify(data),
@@ -31,50 +47,32 @@ export default function Form() {
           </p>
         </div>
         <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleFetch();
-            console.log(data.email);
-            console.log(data.name);
-            console.log(data.lastName);
-          }}
+          onSubmit={handleSubmit(onSubmit)}
           className="flex flex-col justify-between space-y-3 px-6 pb-10 pt-4 lg:px-16 lg:py-10"
         >
           <div className="flex flex-row justify-between">
             <p className="font-semibold">Name</p>
             <input
               className="w-[150px] rounded-sm border border-black bg-[#F8F8F8] md:w-[180px]"
-              type="text"
-              value={data.name}
-              onChange={(event) => {
-                setData({ ...data, name: event.target.value });
-              }}
+              {...register("name")}
             />
+            <p>{errors.name?.message}</p>
           </div>
           <div className="flex flex-row justify-between">
             <p className="font-semibold">Last Name</p>
             <input
               className="w-[150px] rounded-sm border border-black bg-[#F8F8F8] md:w-[180px]"
-              type="text"
-              value={data.lastName}
-              onChange={(event) => {
-                setData({
-                  ...data,
-                  lastName: event.target.value,
-                });
-              }}
+              {...register("lastName")}
             />
+            <p>{errors.lastName?.message}</p>
           </div>
           <div className="flex flex-row justify-between space-x-5">
             <p className="font-semibold">Contact Email</p>
             <input
               className="mb-6 w-[150px] rounded-sm border border-black bg-[#F8F8F8] md:w-[180px]"
-              type="email"
-              value={data.email}
-              onChange={(event) => {
-                setData({ ...data, email: event.target.value });
-              }}
+              {...register("email")}
             />
+            <p>{errors.email?.message}</p>
           </div>
           <Button
             type="submit"
