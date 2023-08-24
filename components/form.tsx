3114d154772f7d29199d5bee87a9f910877fcc28
Alejo Paulon <alejo.paulon@gmail.com>
2023-08-24 09:28:22 -0300

    form styling finished

--- components/form.tsx
+++ components/form.tsx
@@ -13,9 +13,18 @@ const schema = yup
       .string()
       .email("Es necesario poner un mail válido")
       .min(5, "Mínimo 5 caracteres")
+      .max(50, "Máximo 50 caracteres")
       .required(),
-    name: yup.string().min(5, "Mínimo 5 caracteres").required(),
-    lastName: yup.string().min(5, "Mínimo 5 caracteres").required(),
+    name: yup
+      .string()
+      .min(5, "Mínimo 5 caracteres")
+      .max(20, "Máximo 20 caracteres")
+      .required(),
+    lastName: yup
+      .string()
+      .min(5, "Mínimo 5 caracteres")
+      .max(20, "Máximo 20 caracteres")
+      .required(),
   })
   .required();
 type FormData = yup.InferType<typeof schema>;
@@ -36,10 +45,10 @@ export default function Form() {
   };
 
   return (
-    <div className="flex justify-center bg-[#F8F8F8]">
-      <div className="flex flex-col rounded-xl bg-white lg:flex-row lg:justify-between lg:px-40 lg:py-4">
-        <div className="flex flex-col py-10 text-center lg:text-start">
-          <h2 className="px-10 text-2xl font-semibold lg:max-w-xl lg:px-0 lg:text-4xl/normal">
+    <div className="flex min-h-[600px] justify-center bg-[#F8F8F8]">
+      <div className="flex flex-col rounded-xl bg-white lg:flex-row lg:justify-between lg:px-40 lg:py-10 xl:min-w-[1100px] xl:space-x-36">
+        <div className="flex flex-col py-10 text-center lg:text-start xl:max-w-[200px]">
+          <h2 className="px-10 text-2xl font-semibold lg:px-0 lg:text-4xl/normal">
             Subscribe Now To Get Special Features!
           </h2>
           <p className="mt-6 max-w-lg leading-8">
@@ -48,45 +57,49 @@ export default function Form() {
         </div>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col justify-between space-y-3 px-6 pb-10 pt-4 lg:px-16 lg:py-10"
+          className="flex flex-col items-center space-y-3 pb-10 pt-4 lg:px-16 lg:py-10"
         >
-          <div className="flex flex-row justify-between">
-            <p className="font-semibold">Name</p>
-            <div className="flex flex-col">
+          <div className="flex flex-col items-center">
+            <p className="py-2 font-semibold">Name</p>
+            <div className="flex flex-col items-center">
               <input
                 className="w-[150px] rounded-sm border border-black bg-[#F8F8F8] md:w-[180px]"
                 {...register("name")}
               />
-              <p className="mt-2">{errors.name?.message}</p>
+              <p className="mt-2 text-center italic">{errors.name?.message}</p>
             </div>
           </div>
-          <div className="flex flex-row justify-between">
-            <p className="font-semibold">Last Name</p>
-            <div className="flex flex-col">
+          <div className="flex flex-col items-center">
+            <p className="py-2 font-semibold">Last Name</p>
+            <div className="flex flex-col items-center">
               <input
                 className="w-[150px] rounded-sm border border-black bg-[#F8F8F8] md:w-[180px]"
                 {...register("lastName")}
               />
-              <p className="mt-2">{errors.lastName?.message}</p>
+              <p className="mt-2 text-center italic">
+                {errors.lastName?.message}
+              </p>
             </div>
           </div>
-          <div className="flex flex-row justify-between space-x-5">
-            <p className="font-semibold">Contact Email</p>
-            <div className="flex flex-col">
+          <div className="flex flex-col items-center">
+            <p className="py-2 font-semibold">Contact Email</p>
+            <div className="flex flex-col items-center">
               <input
-                className="mb-6 w-[150px] rounded-sm border border-black bg-[#F8F8F8] md:w-[180px]"
+                className="w-[150px] rounded-sm border border-black bg-[#F8F8F8] md:w-[180px]"
                 {...register("email")}
               />
-              <p className="mt-2">{errors.email?.message}</p>
+              <p className="mt-2 text-center italic">{errors.email?.message}</p>
             </div>
           </div>
-          <Button
-            type="submit"
-            className="flex h-12 w-56 self-end"
-            variant={"filled_red"}
-          >
-            Subscribe Now
-          </Button>
+          <div className="lg:pt-6">
+            <Button
+              type="submit"
+              className="flex h-12 w-56 lg:self-end"
+              variant={"filled_red"}
+            >
+              Subscribe Now
+            </Button>
+          </div>
         </form>
       </div>
     </div>
